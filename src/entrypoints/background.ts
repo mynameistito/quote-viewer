2dabba590478a427d55e0aafd627796b13a5735f
Tito <github@mynameistito.com>
2026-05-25 19:11:26 +1200

    feat: remove background script and onClicked listener

--- src/entrypoints/background.ts
+++
@@ -1,9 +0,0 @@
-import { defineBackground } from "#imports";
-
-export default defineBackground(() => {
-  browser.action.onClicked.addListener(() => {
-    void browser.tabs.create({
-      url: "https://github.com/mynameistito/quote-viewer",
-    });
-  });
-});
